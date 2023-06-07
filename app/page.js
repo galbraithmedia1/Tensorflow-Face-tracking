f7ccba893ee2a675195f3febd9e3183aee879984
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2023-06-07 17:20:48 -0600

    upload

--- app/page.js
+++ app/page.js
@@ -1,113 +1,113 @@
-import Image from 'next/image'
+"use client";
+
+import React, { useRef, useState, useEffect } from "react";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs";
+import "./App.css";
 
 export default function Home() {
+  const videoRef = useRef();
+  const [model, setModel] = useState(undefined);
+  const [children, setChildren] = useState([]);
+
+  useEffect(() => {
+    (async function loadModel() {
+      const loadedModel = await cocoSsd.load();
+      setModel(loadedModel);
+      // await model.save("downloads://my-model-1");
+      // console.log(JSON.stringify(await tf.io.listModels()));
+    })();
+  }, []);
+  // console.log("model", model);
+
+  const enableCam = (event) => {
+    if (!model) {
+      return;
+    }
+    event.target.classList.add("removed");
+    const constraints = {
+      video: true,
+    };
+    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+      videoRef.current.srcObject = stream;
+      videoRef.current.addEventListener("loadeddata", predictWebcam);
+    });
+  };
+
+  // save the model
+  const saveModel = async () => {
+    await model.save("downloads://my-model-1");
+    console.log(JSON.stringify(await tf.io.listModels()));
+  };
+
+  const predictWebcam = () => {
+    model.detect(videoRef.current).then(function (predictions) {
+      const newChildren = [];
+      for (let n = 0; n < predictions.length; n++) {
+        if (predictions[n].score > 0.66) {
+          const pText =
+            predictions[n].class +
+            " - with " +
+            Math.round(parseFloat(predictions[n].score) * 100) +
+            "% confidence.";
+          newChildren.push({
+            bbox: predictions[n].bbox,
+            text: pText,
+          });
+        }
+      }
+      setChildren(newChildren);
+      window.requestAnimationFrame(predictWebcam);
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <h1>
+        Multiple object detection using pre trained model in TensorFlow.js
+      </h1>
+      <p>
+        Wait for the model to load before clicking the button to enable the
+        webcam - at which point it will become visible to use.
+      </p>
+      <div id="liveView" className="camView">
+        <button id="webcamButton" onClick={enableCam}>
+          Enable Webcam
+        </button>
+        <video
+          id="webcam"
+          ref={videoRef}
+          autoPlay
+          muted
+          width="640"
+          height="480"
+        ></video>
+        {children.map((child, i) => (
+          <div
+            key={i}
+            className="highlighter"
+            style={{
+              left: child.bbox[0],
+              top: child.bbox[1],
+              width: child.bbox[2],
+              height: child.bbox[3],
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            <p
+              style={{
+                marginLeft: child.bbox[0],
+                marginTop: child.bbox[1] - 10,
+                width: child.bbox[2] - 10,
+              }}
+            >
+              {child.text}
+            </p>
+          </div>
+        ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <button id="saveModel" onClick={saveModel}>
+        Save Model
+      </button>
+    </div>
+  );
 }
